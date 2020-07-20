3e37c000d910c679a499e09ab044b9ec35600191
Kaishiyoku <dmp@nfze.de>
2020-07-20 12:47:56 +0200

    added React skeleton

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import React from 'react';
+import {render} from 'react-dom';
+
+const rootEl = document.getElementById('root');
+
+const App = () => (
+    <div>
+        TODO
+    </div>
+);
+
+render(<App/>, rootEl);
