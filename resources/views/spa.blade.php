d65e7c17d5a6a23d5a369231c89d3948030a386d
Kaishiyoku <dmp@nfze.de>
2020-07-23 23:50:14 +0200

    added HTML meta tag for mobile devices

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+        <meta name="mobile-web-app-capable" content="yes">
 
         <title>{{ env('APP_NAME', 'Laravel') }}</title>
 
@@ -19,6 +20,25 @@
     <body>
 
     <div id="root"></div>
+
+    <script type="text/javascript">
+        function toggleFullScreen() {
+            var doc = window.document;
+            var docEl = doc.documentElement;
+
+            var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+            var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+            if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+                requestFullScreen.call(docEl);
+            }
+            else {
+                cancelFullScreen.call(doc);
+            }
+        }
+
+        document.body.requestFullscreen();
+    </script>
 
     </body>
 </html>
