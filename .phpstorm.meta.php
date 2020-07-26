78902230bc199d719d58f07e46c5f784990ca02e
Kaishiyoku <dmp@nfze.de>
2020-07-26 18:06:18 +0200

    updated Composer dependencies

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-07-25 22:56:10.
+    * Generated on 2020-07-26 18:04:50.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
